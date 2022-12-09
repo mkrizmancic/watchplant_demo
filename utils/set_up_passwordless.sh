4e97e3a79a7f8eaf1a784c83322b80db95da5aa0
Marko Krizmancic <marko.krizmancic@fer.hr>
2022-12-09 13:05:37 +0100

    Add util scripts for running from docker

---
+++ utils/set_up_passwordless.sh
@@ -0,0 +1,5 @@
+echo "This script will generate a key for passwordless ssh onto Raspberries."
+echo "On each prompt just press enter key to confirm the defaults."
+sleep 2
+
+ssh-keygen -t rsa
---
+++ utils/update_bashrc_remote.sh
@@ -0,0 +1,8 @@
+ip=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
+
+nodes="0 1 2 3 7"
+
+for val in $nodes; do
+    echo "Modifying rpi$val."
+    ssh pi@rpi"$val".local "sed -ie 's/export ROS_MASTER_URI.*/export ROS_MASTER_URI=http:\/\/$ip:11311/g' ~/.bashrc"
+done
